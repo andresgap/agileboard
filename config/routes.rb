5b9a9f7537cd332f474d9366acd2ca36afd29ac2
drusba <alejandro.garrido.g@gmail.com>
2017-01-06 09:09:18 -0400

    Users login and Users Admin Roles

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  get 'home/index'
+
+  devise_for :users, controllers: {
+      sessions: 'users/sessions',
+      passwords: 'users/passwords',
+      registrations: 'users/registrations'
+  }
+  #devise_for :users
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "home#index"
+end
